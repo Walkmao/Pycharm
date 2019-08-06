24eeee2481172212aa82177269a8d946d7989d18
Walkmao <1830926109@qq.com>
2019-08-06 23:33:04 +0800

    update

--- Data_processing/Danmu_ciyun/Blibli.py
+++ Data_processing/Danmu_ciyun/Blibli.py
@@ -4,6 +4,8 @@
 # datetime:2019/8/6 17:25
 # software: PyCharm
 
+#上海垃圾分类
+
 #方法一：随意打开一个B站视频，按F12，点开network按键，在左上方的输入区输入 list 便可找到相应的弹幕数据包。
 
 #方法二：http://comment.bilibili.com/cid.xml  {99768393}
@@ -20,8 +22,10 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import jieba
+from PIL import Image
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud,ImageColorGenerator
+import numpy as np
 
 #请求弹幕数据
 url = 'http://comment.bilibili.com/99768393.xml'
@@ -40,7 +44,8 @@ df = pd.DataFrame(comments_dict)
 df.to_csv('blibili.csv',encoding='utf-8')
 
 # 解析背景图片
-mask_img = plt.imread('Bulb.jpg')
+mask_img = plt.imread('test.jpg')           #背景大小
+mask_img2 = np.array(Image.open('2.png'))   #用来设定和限制形状
 
 #设置词云样式
 wc = WordCloud(
@@ -51,7 +56,7 @@ wc = WordCloud(
     # 设置最大号字体大小
     max_font_size=80,
     # 设置使用的背景图片
-    mask=mask_img,
+    mask=mask_img2,
     # 设置输出的图片背景色
     background_color=None, mode="RGBA",
     # 设置有多少种随机生成状态，即有多少种配色方案
@@ -65,6 +70,7 @@ text = ''
 for line in br[1]:
     text += ' '.join(jieba.cut(line, cut_all=False))
 
+wc.generate(text)
 plt.imshow(wc)
-plt.axis('off')
+plt.axis('off')     # 关闭坐标轴
 plt.show()
