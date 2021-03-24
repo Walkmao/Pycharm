ba10826d92179fbbe890105ba4bf36076444c172
Busycat <1830926109@qq.com>
2021-03-24 19:32:03 +0800

    update

--- Python自动化办公/row_height.py
+++ Python自动化办公/row_height.py
@@ -1,12 +1,12 @@
 import os
 from openpyxl import load_workbook
 
-filePath = r"E:\TodoList\GBW\test"
+filePath = r"E:\TodoList\GBW\东区2020汇总体检报告"
 for dirpath, dirnames, filenames in os.walk(filePath):
     path = [os.path.join(dirpath, names) for names in filenames]
 for filename in path:
     workbook = load_workbook(filename=filename)
     sheet = workbook['Sheet1']
     for i in range(42):
-        sheet.row_dimensions[i].height = 18
+        sheet.row_dimensions[i].height = 18     # 调整列宽ws.column_dimensions['A'].width = 20.0
     workbook.save(filename)
